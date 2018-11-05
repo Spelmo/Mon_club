232f01cd90f5c6ca8a6fb5e2b4bc61fe7398524b
MC Le Morvan <mclemorvan@gmail.com>
2018-11-05 14:33:54 +0100

    added signup and log in features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def new
+   @user = User.new
   end
+
+  def show
+  @user = User.find(params[:id])
+  end
+
+  def create
+   params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+   @user = User.new(user_params)
+   if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
+   else
+     render 'new'
+   end
+ end
+
+   private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
